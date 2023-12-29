fae84757bd44b8963d5e9084135b91a86060c16d
Jason Ray <jayray.net@gmail.com>
2023-12-29 14:33:51 -0500

    format

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-customers = [
-    {"id": 1, "name": "Alice"},
-    {"id": 2, "name": "Bob"},
-    {"id": 3, "name": "Charlie"}
-]
+customers = [{
+    "id": 1,
+    "name": "Alice"
+}, {
+    "id": 2,
+    "name": "Bob"
+}, {
+    "id": 3,
+    "name": "Charlie"
+}]
 
 
 # Define a route to return the list of customers
@@ -14,6 +19,7 @@ customers = [
 @app.route('/customers/', methods=['GET'])
 def get_customers():
     return jsonify(customers)
+
 
 # Define a route to return a single customer by ID
 @app.route('/customers/<int:customer_id>', methods=['GET'])
@@ -24,5 +30,6 @@ def get_customer(customer_id):
     else:
         return jsonify({"error": "Customer not found"}), 404
 
+
 if __name__ == '__main__':
-    app.run(debug=True , host="127.0.0.1", port=8081)
+    app.run(debug=True, host="127.0.0.1", port=8081)
